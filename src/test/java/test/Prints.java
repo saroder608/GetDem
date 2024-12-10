f95fdc0494be643c00bd04f0fcb6455a5bb988a8
rohit <saroder608@gmail.com>
2024-12-10 16:27:13 +0530

    first time commit

---
+++ src/test/java/test/Prints.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Prints {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("first print"); 
+		System.out.println("second print"); 
+
+	}
+
+}
